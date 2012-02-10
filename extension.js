a36cd6bd03f24c6370130f24f030188cdfac2cab
A. B <arnaud.bonatti@gmail.com>
2012-02-10 09:26:34 +0100

    retire les minutes à heure pile

--- extension.js
+++ extension.js
@@ -30,82 +30,89 @@ const MARGIN = 1;
 
 
 // in org.gnome.desktop.interface
-const CLOCK_FORMAT_KEY        = 'clock-format';
+const CLOCK_FORMAT_KEY		= 'clock-format';
 // in org.gnome.shell.clock
 const CLOCK_SHOW_SECONDS_KEY  = 'show-seconds';
 
 
 function NewClock() {
-    this._init();
+	this._init();
 }
 
 NewClock.prototype = {
-    _init: function() {
-	this.date = Main.panel._dateMenu._date;
-	// this.settings = Main.panel._dateMenu.menu.box.get_children()[0].get_children()[0].get_children()[3];
-            
-        this.display_time = [-1, -1];
-        this.time_format = "%R"; // Safe fallback
-        
-        this.date_menu = Main.panel._dateMenu;
-        this.orig_clock = this.date_menu._clock;
-        this.new_clock = new St.Label({text: ERROR_LABEL});
-        
-        this.desktop_settings = new Gio.Settings({ schema: "org.gnome.desktop.interface" });
-        this.clock_settings = new Gio.Settings({ schema: "org.gnome.shell.clock" });
-        this.desktop_settings.connect("changed", Lang.bind(this, this.update_format));
-        this.clock_settings.connect("changed", Lang.bind(this, this.update_format));
-        this.update_format();
-    },
-    
-    Run: function() {
-        this.run = true;
-        this.on_timeout();
-        Mainloop.timeout_add(UPDATE_INTERVAL, Lang.bind(this, this.on_timeout));  
-    },
-    
-    update_format: function() {
-        let clock_format = this.desktop_settings.get_string(CLOCK_FORMAT_KEY);
-        let show_seconds = this.clock_settings.get_boolean(CLOCK_SHOW_SECONDS_KEY);
-        
-        if (clock_format == "24h") this.time_format = "%-H h %-M";
-        else this.time_format = "%a %e-%m, %l:%M";
-        
-        if (show_seconds) this.time_format += ":%S";
-        
-        if (clock_format == "24h") this.time_format += "    %A %e";
-        else this.time_format += " %p";
-    },
-    
-    on_timeout: function() {
-        let now = new Date();
-        this.new_clock.set_text(now.toLocaleFormat(this.time_format))
-        let display_time = [now.getHours(), now.getMinutes()];
-        
-        if ((this.display_time[0] != display_time[0]) || (this.display_time[1] != display_time[1])) {
-            this.display_time = display_time;
-        }
-        
-        return true;
-    },
-    
-    enable: function() {
-        this.date_menu.actor.remove_actor(this.orig_clock);
-        this.date_menu.actor.add_actor(this.new_clock);
-        
-        this.Run();
-	this.date.hide();
-    },
-    
-    disable: function() {
-	this.date.show();
-        this.run = false;
-        
-        this.date_menu.actor.remove_actor(this.new_clock);
-        this.date_menu.actor.add_actor(this.orig_clock);
-    }
+	_init: function() {
+		this.date = Main.panel._dateMenu._date;
+		// this.settings = Main.panel._dateMenu.menu.box.get_children()[0].get_children()[0].get_children()[3];
+		
+		this.time_format = "%R";	// Safe fallback
+		this.time_format_if = "%R";	// O'clock
+		
+		this.date_menu = Main.panel._dateMenu;
+		this.orig_clock = this.date_menu._clock;
+		this.new_clock = new St.Label({text: ERROR_LABEL});
+		
+		this.desktop_settings = new Gio.Settings({ schema: "org.gnome.desktop.interface" });
+		this.clock_settings = new Gio.Settings({ schema: "org.gnome.shell.clock" });
+		this.desktop_settings.connect("changed", Lang.bind(this, this.update_format));
+		this.clock_settings.connect("changed", Lang.bind(this, this.update_format));
+		this.update_format();
+	},
+	
+	Run: function() {
+		this.run = true;
+		this.on_timeout();
+		Mainloop.timeout_add(UPDATE_INTERVAL, Lang.bind(this, this.on_timeout));  
+	},
+	
+	update_format: function() {
+		let clock_format = this.desktop_settings.get_string(CLOCK_FORMAT_KEY);
+		let show_seconds = this.clock_settings.get_boolean(CLOCK_SHOW_SECONDS_KEY);
+		
+		if (clock_format == "24h") {
+			if (show_seconds) {
+				this.time_format = "%-H h %-M m %S s";
+				this.time_format_if = "%-H h %-M m %S s";
+			}else{
+				this.time_format = "%-H h %-M";
+				this.time_format_if = "%-H h";
+			}
+			this.time_format += "    %A %e";
+			this.time_format_if += "    %A %e";
+		}else{	
+			this.time_format = "%a %e-%m, %l:%M";
+			this.time_format_if = "%a %e-%m, %l:%M";
+			if (show_seconds) this.time_format += "%S";
+			if (show_seconds) this.time_format_if += "%S";
+			this.time_format += " %p";
+			this.time_format_if += " %p";
+		}
+	},
+	
+	on_timeout: function() {
+		let now = new Date();
+		if (now.getMinutes()==0) this.new_clock.set_text(now.toLocaleFormat(this.time_format_if));
+		else this.new_clock.set_text(now.toLocaleFormat(this.time_format));
+		
+		return true;
+	},
+	
+	enable: function() {
+		this.date_menu.actor.remove_actor(this.orig_clock);
+		this.date_menu.actor.add_actor(this.new_clock);
+		
+		this.Run();
+		this.date.hide();
+	},
+	
+	disable: function() {
+		this.date.show();
+		this.run = false;
+		
+		this.date_menu.actor.remove_actor(this.new_clock);
+		this.date_menu.actor.add_actor(this.orig_clock);
+	}
 }
 
 function init() {
-    return new NewClock();
+	return new NewClock();
 }
